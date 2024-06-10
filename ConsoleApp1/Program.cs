f80f77eede77942dac7c7f90f5c5b64850a57dde
Ziya Suzen <ziya@suzen.net>
2024-06-10 19:45:42 +0100

    :zap:

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using NATS.Client;
+using NATS.Client.JetStream;
+
+var connection = new ConnectionFactory().CreateConnection("nats://127.0.0.1:4222");
+var jsm = connection.CreateJetStreamManagementContext();
+
+// nats s create s1 --subjects s1 --defaults
+var info = jsm.GetStreamInfo("s1");
+
+var newConfig = new StreamConfiguration.StreamConfigurationBuilder(info.Config)
+    .WithMaxAge(300_000) // ms
+    .WithReplicas(2)
+    .Build();
+
+jsm.UpdateStream(newConfig);
+
+
+// nats s info s1
+//     Maximum Age: 5m0s
+
+Console.WriteLine(FileVersionInfo.GetVersionInfo(typeof(ConnectionFactory).Assembly.Location).FileVersion);
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using NATS.Client;
+using NATS.Client.JetStream;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connection = new ConnectionFactory().CreateConnection("nats://127.0.0.1:4222");
+            var jsm = connection.CreateJetStreamManagementContext();
+
+            // nats s create s1 --subjects s1 --defaults
+            var info = jsm.GetStreamInfo("s1");
+
+            var newConfig = new StreamConfiguration.StreamConfigurationBuilder(info.Config)
+                .WithMaxAge(600_000) // ms
+                .WithReplicas(3)
+                .Build();
+
+            jsm.UpdateStream(newConfig);
+
+
+            // nats s info s1
+            //     Maximum Age: 5m0s
+
+            Console.WriteLine(FileVersionInfo.GetVersionInfo(typeof(ConnectionFactory).Assembly.Location).FileVersion);
+        }
+    }
+}
